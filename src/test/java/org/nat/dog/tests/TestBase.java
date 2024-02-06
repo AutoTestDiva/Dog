7651bf738cc75f3cc5aa6760e5a5741d2f99b1b6
AutoTestDiva <153229617+AutoTestDiva@users.noreply.github.com>
2024-02-06 20:06:51 +0100

    Update TestBase.java

--- src/test/java/org/nat/dog/tests/TestBase.java
+++ src/test/java/org/nat/dog/tests/TestBase.java
@@ -38,7 +38,7 @@ public class TestBase {
         }
         WebDriverListener listener = new MyListener();
         driver = new EventFiringDecorator(listener).decorate(driver);
-        driver.get("https://hunde24.online/front/");
+        driver.get("https://dog-app-iw6ow.ondigitalocean.app/");
         logger.info("The link --> " + driver.getCurrentUrl());
         logger.info("***********************************");
         driver.manage().window().maximize();
